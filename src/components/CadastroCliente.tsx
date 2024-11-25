20ee977c263602e95141bcfa92934b1b77a083fa
Devwillmarinho <willmarinho97@gmail.com>
2024-11-24 22:44:07 -0300

    incusão da aba CadastroCliente para remover erro no codigo...

---
+++ src/components/CadastroCliente.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const CadastroCliente = ({ user, onCadastro }) => {
+  // Lógica do componente
+  return <div>Cadastro Cliente</div>;
+};
+
+export default CadastroCliente;
